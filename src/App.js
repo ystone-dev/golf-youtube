cd890077dc699b8ed8716044b9e5ab028235a3ae
이윤석 <ystone0213@naver.com>
2023-11-01 21:26:47 +0900

    feat: 페이지 구성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Channel from "./pages/Channel";
+import Developer from "./pages/Developer";
+import Gsap from "./pages/Gsap";
+import Home from "./pages/Home";
+import Not from "./pages/Not";
+import Port from "./pages/Port";
+import Search from "./pages/Search";
+import Today from "./pages/Today";
+import Video from "./pages/Video";
+import Webd from "./pages/Webd";
+import Website from "./pages/Website";
+import Youtube from "./pages/Youtube";
 
 const App = () => {
-  return <div>asdasd</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/today" element={<Today />} />
+        <Route path="/developer" element={<Developer />} />
+        <Route path="/webd" element={<Webd />} />
+        <Route path="/website" element={<Website />} />
+        <Route path="/gsap" element={<Gsap />} />
+        <Route path="/port" element={<Port />} />
+        <Route path="/youtube" element={<Youtube />} />
+        <Route path="/channel/:channelId" element={<Channel />} />
+        <Route path="/video/:videoId" element={<Video />} />
+        <Route path="/search/:searchId" element={<Search />} />
+        <Route path="*" element={<Not />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
