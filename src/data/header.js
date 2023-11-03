07a66712afcca5ff6cf3d9a4c68ddd93e36f94b3
이윤석 <ystone0213@naver.com>
2023-11-03 18:25:49 +0900

    feat: header 데이터 추가

---
+++ src/data/header.js
@@ -0,0 +1,24 @@
+export const headerMenus = [
+  { title: "홈페이지", src: "/" },
+  { title: "추천 영상", src: "/today" },
+  { title: "추천 레스너", src: "/lessoner" },
+  { title: "어드레스", src: "/address" },
+  { title: "백스윙", src: "/back" },
+  { title: "다운스윙", src: "/down" },
+  { title: "어프로치", src: "/approach" },
+  { title: "퍼팅", src: "/putting" },
+];
+
+export const searchKeyword = [
+  { title: "그립", src: "/search/그립" },
+  { title: "테이크백", src: "/search/테이크백" },
+  { title: "트랜지션", src: "/search/트랜지션" },
+  { title: "임팩트", src: "/search/임팩트" },
+  { title: "팔로스루", src: "/search/팔로스루" },
+  { title: "피니쉬", src: "/search/피니쉬" },
+  { title: "드라이버", src: "/search/드라이버" },
+  { title: "우드", src: "/search/우드" },
+  { title: "유틸리티", src: "/search/유틸리티" },
+  { title: "아이언", src: "/search/아이언" },
+  { title: "벙커샷", src: "/search/벙커샷" },
+];
