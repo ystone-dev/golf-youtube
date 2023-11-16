97ef7f17328104f30d0f9f3d8cbabe6b1244e27f
이윤석 <ystone0213@naver.com>
2023-11-16 18:52:53 +0900

    feat: down 추가

--- src/components/contents/Down.jsx
+++ src/components/contents/Down.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { downText } from "../../data/down";
+import { Link } from "react-router-dom";
 
-const Down = () => {
-  return <div>Down</div>;
+const Back = () => {
+  return (
+    <section className="video">
+      <h2>🏌🏻 다운스윙</h2>
+      <div className="container">
+        {downText.map((video, key) => (
+          <div key={key}>
+            <div className="thumbnail icon--play">
+              <Link to={`/video/${video.videoId}`}>
+                <img src={video.img} alt={video.title} />
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
-export default Down;
+export default Back;
---
+++ src/data/down.js
@@ -0,0 +1,13 @@
+import down01 from "../assets/img/down01.jpeg";
+
+export const downText = [
+  {
+    title:
+      "[다운스윙/ 팔꿈치넣기] 프로들의 어깨 외회전하는 방법과 팔꿈치가 들어가는 원리",
+    img: down01,
+    author: "박연습TV",
+    videoId: "MiISLxCF3dw",
+    date: "2023. 11. 12.",
+    channelId: "UC6sVX_-lnuq8LqsD7Lp4WMg",
+  },
+];
