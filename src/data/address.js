542e1dfb3bc86a0fd770b7cdf7b8f1cd6060f777
이윤석 <ystone0213@naver.com>
2023-11-16 18:51:35 +0900

    feat: address 추가, 스타일링

---
+++ src/data/address.js
@@ -0,0 +1,12 @@
+import address01 from "../assets/img/address01.jpeg";
+
+export const addressText = [
+  {
+    title: "투어프로가 되면 처음부터 배우는 어드레스 방법! 비거리 폭발의 시작",
+    img: address01,
+    author: "어썸골프 Awesome Golf",
+    videoId: "kX8LxWrG-eU",
+    date: "2023. 9. 5.",
+    channelId: "UCCgn6a16Ba1N9gif8opQXoQ",
+  },
+];
