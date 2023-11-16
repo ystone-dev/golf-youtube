28ae299ad6b2a6e09a251d71aa1bd29346c93b63
이윤석 <ystone0213@naver.com>
2023-11-16 20:04:39 +0900

    feat: address 데이터 추가

--- src/data/address.js
+++ src/data/address.js
@@ -1,4 +1,8 @@
 import address01 from "../assets/img/address01.jpeg";
+import address02 from "../assets/img/address02.jpeg";
+import address03 from "../assets/img/address03.jpeg";
+import address04 from "../assets/img/address04.jpeg";
+import address05 from "../assets/img/address05.jpeg";
 
 export const addressText = [
   {
@@ -8,5 +12,38 @@ export const addressText = [
     videoId: "kX8LxWrG-eU",
     date: "2023. 9. 5.",
     channelId: "UCCgn6a16Ba1N9gif8opQXoQ",
+  },
+  {
+    title: "[아카데미 박진이] 어드레스 시 왼손목의 꺾임을 만들기",
+    img: address02,
+    author: "SBS Golf",
+    videoId: "26JJ16G-_Dg",
+    date: "2021. 10. 27.",
+    channelId: "UC12XQWcIlaavjjbhh6EeMJw",
+  },
+  {
+    title: "국대골프 // 올바른 백스윙 궤도는 팔꿈치가 결정합니다",
+    img: address03,
+    author: "국대골프",
+    videoId: "A1lk0b1FKCU",
+    date: "2021. 11. 20.",
+    channelId: "UC1xP40gA7K64YkFJF9BePaA",
+  },
+  {
+    title: "[골프 레슨] 재미없는 어드레스 레슨",
+    img: address04,
+    author: "이보연프로 LBYGOLF",
+    videoId: "Xg1BeaC6lGI",
+    date: "2022. 7. 26.",
+    channelId: "UCRMRStGx4lDEhDHJb0LTTNA",
+  },
+  {
+    title:
+      "흔들리는 드라이버 샷은 그만! 프로들이 하는 어드레스 세팅법 대공개!!",
+    img: address05,
+    author: "[ER 골프] 이정웅프로",
+    videoId: "8lK83qxxXKc",
+    date: "2023. 1. 21.",
+    channelId: "UCLdLy47SvLDgHkO4F1HcAWw",
   },
 ];
