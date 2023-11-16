411e08c7ff5bf617728818fb4656834ae09427b2
이윤석 <ystone0213@naver.com>
2023-11-16 18:53:16 +0900

    feat: putting 추가

--- src/components/contents/Putting.jsx
+++ src/components/contents/Putting.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { puttingText } from "../../data/putting";
+import { Link } from "react-router-dom";
 
-const Putting = () => {
-  return <div>Putting</div>;
+const Back = () => {
+  return (
+    <section className="video">
+      <h2>🏌🏻 퍼팅</h2>
+      <div className="container">
+        {puttingText.map((video, key) => (
+          <div key={key}>
+            <div className="thumbnail icon--play">
+              <Link to={`/video/${video.videoId}`}>
+                <img src={video.img} alt={video.title} />
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
-export default Putting;
+export default Back;
---
+++ src/data/putting.js
@@ -0,0 +1,21 @@
+import putting01 from "../assets/img/putting01.jpeg";
+import putting02 from "../assets/img/putting02.jpeg";
+
+export const puttingText = [
+  {
+    title: "[아카데미 이시우] 어드레스부터 그립까지 손목 체크",
+    img: putting01,
+    author: "SBS Golf 레슨",
+    videoId: "Cp2duhBNu6s",
+    date: "2022. 12. 14.",
+    channelId: "UCnY8QXU95EhOiPj30e9Maqg",
+  },
+  {
+    title: "이시우 프로 퍼팅 비법 대공개!!!! 골린이 주목!!",
+    img: putting02,
+    author: "이시우의 골프생",
+    videoId: "-UWcZAefXwg",
+    date: "2021. 12. 24.",
+    channelId: "UChbsjh2xP66KxNjkAJoDImw",
+  },
+];
