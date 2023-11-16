15a50e8f3d317a11446641cef42a4fc16391cba2
이윤석 <ystone0213@naver.com>
2023-11-16 18:52:03 +0900

    feat: approach 추가

--- src/components/contents/Approach.jsx
+++ src/components/contents/Approach.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { approachText } from "../../data/approach";
+import { Link } from "react-router-dom";
 
-const Approach = () => {
-  return <div>Approach</div>;
+const Back = () => {
+  return (
+    <section className="video">
+      <h2>🏌🏻 어프로치</h2>
+      <div className="container">
+        {approachText.map((video, key) => (
+          <div key={key}>
+            <div className="thumbnail icon--play">
+              <Link to={`/video/${video.videoId}`}>
+                <img src={video.img} alt={video.title} />
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
-export default Approach;
+export default Back;
---
+++ src/data/approach.js
@@ -0,0 +1,13 @@
+import approach01 from "../assets/img/approach01.jpeg";
+
+export const approachText = [
+  {
+    title:
+      "짧은 어프로치는 이 영상이 근본~! 정말 쉽고, 딱딱~ 붙네요~ 붙어~![골프레슨]",
+    img: approach01,
+    author: "배재희프로의오늘의골프",
+    videoId: "tMYeR7krqRA",
+    date: "2023. 10. 23.",
+    channelId: "UCGdqV9ElP2_WXBEjOkZAqug",
+  },
+];
