03e5fa026cd5b0e866eabbffde76151c0d413369
이윤석 <ystone0213@naver.com>
2023-11-16 18:52:30 +0900

    feat: back 추가

--- src/components/contents/Back.jsx
+++ src/components/contents/Back.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { backText } from "../../data/back";
+import { Link } from "react-router-dom";
 
 const Back = () => {
-  return <div>Back</div>;
+  return (
+    <section className="video">
+      <h2>🏌🏻 백스윙</h2>
+      <div className="container">
+        {backText.map((video, key) => (
+          <div key={key}>
+            <div className="thumbnail icon--play">
+              <Link to={`/video/${video.videoId}`}>
+                <img src={video.img} alt={video.title} />
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Back;
---
+++ src/data/back.js
@@ -0,0 +1,12 @@
+import back01 from "../assets/img/back01.jpeg";
+
+export const backText = [
+  {
+    title: "최힌지 프로가 알려주는⛳ 힌지를 이용한 아이언 백스윙",
+    img: back01,
+    author: "최민욱의 스마일골프",
+    videoId: "YHzSrxJxJxg",
+    date: "2023. 10. 11.",
+    channelId: "UCTlAS_N5_SmgJqwF_o5jqfg",
+  },
+];
