344d168e9bbb427a5ba251521e10f3eaabd72759
이윤석 <ystone0213@naver.com>
2023-11-17 18:29:37 +0900

    feat: back 내 데이터 추가

--- src/data/back.js
+++ src/data/back.js
@@ -1,4 +1,9 @@
 import back01 from "../assets/img/back01.jpeg";
+import back02 from "../assets/img/back02.jpeg";
+import back03 from "../assets/img/back03.jpeg";
+import back04 from "../assets/img/back04.jpeg";
+import back05 from "../assets/img/back05.jpeg";
+import back06 from "../assets/img/back06.jpeg";
 
 export const backText = [
   {
@@ -8,5 +13,47 @@ export const backText = [
     videoId: "YHzSrxJxJxg",
     date: "2023. 10. 11.",
     channelId: "UCTlAS_N5_SmgJqwF_o5jqfg",
+  },
+  {
+    title:
+      "현대식 백스윙도 모르면서 레슨하는 프로들때문에 격노하신 골프의신 (골프의신레슨 23 - 31)",
+    img: back02,
+    author: "뉴욕홍빠Hong Bar",
+    videoId: "GdqmxXoPa1o",
+    date: "2023. 8. 18.",
+    channelId: "UCNJGbj6_TDIPw3f0UtpqyZg",
+  },
+  {
+    title: "이 원리만 이해하세요, 백스윙 스트레스 사라집니다.",
+    img: back03,
+    author: "[감성골프] 최원진프로",
+    videoId: "K705s0won8Y",
+    date: "2023. 7. 12.",
+    channelId: "UCEBqiMfpvbXTh0X3ovc1a5w",
+  },
+  {
+    title: "틸트&턴 백스윙 회전은 이거 두개만 알면 됩니다.",
+    img: back04,
+    author: "[감성골프] 최원진프로",
+    videoId: "XaunSjWbRaM",
+    date: "2023. 9. 3.",
+    channelId: "UCEBqiMfpvbXTh0X3ovc1a5w",
+  },
+  {
+    title:
+      "[공태현 레슨] 드라이버 백스윙 시 등과 오른팔 위치를 배우면 드라이버 비거리 UP!",
+    img: back05,
+    author: "SBS Golf 레슨",
+    videoId: "WlRwloOO3AY",
+    date: "2023. 7. 28.",
+    channelId: "UCnY8QXU95EhOiPj30e9Maqg",
+  },
+  {
+    title: "가볍게쳐도 볼스피드 70m/s 나오는 방법. '이것'만 바꿔보세요!",
+    img: back06,
+    author: "골프 잘치는 방법 : 땡그랑TV",
+    videoId: "9igL0sE3iuI",
+    date: "2023. 10. 26.",
+    channelId: "UCOFWGUzmNH65HUPxWeTJmYw",
   },
 ];
