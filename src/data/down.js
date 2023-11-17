63b64e24d9226bebdea4fb24f7c78e9859ea3cec
이윤석 <ystone0213@naver.com>
2023-11-17 18:29:50 +0900

    feat: down 내 데이터 추가

--- src/data/down.js
+++ src/data/down.js
@@ -1,4 +1,9 @@
 import down01 from "../assets/img/down01.jpeg";
+import down02 from "../assets/img/down02.jpeg";
+import down03 from "../assets/img/down03.jpeg";
+import down04 from "../assets/img/down04.jpeg";
+import down05 from "../assets/img/down05.jpeg";
+import down06 from "../assets/img/down06.jpeg";
 
 export const downText = [
   {
@@ -9,5 +14,48 @@ export const downText = [
     videoId: "MiISLxCF3dw",
     date: "2023. 11. 12.",
     channelId: "UC6sVX_-lnuq8LqsD7Lp4WMg",
+  },
+  {
+    title: "릴리즈 이 한편으로 평생 끝낼 수 있습니다 조진형프로 영업비밀!",
+    img: down02,
+    author: "조진형프로",
+    videoId: "uUhefRXBFWY",
+    date: "2023. 11. 7.",
+    channelId: "UCmRhM31LOMgqkgAn9ToC2jQ",
+  },
+  {
+    title:
+      "프로 스윙이 되고 싶으시면 왼팔부터 돌릴 줄 알아야 합니다! 왼팔 로테이션!",
+    img: down03,
+    author: "박창현 골프TV",
+    videoId: "mKz9K3m79Pw",
+    date: "2023. 9. 29.",
+    channelId: "UCPgnJDUwZE-uHeeoRAxlM0w",
+  },
+  {
+    title:
+      "[골프레슨] 골프스윙에 있어 가장 큰 터닝포인트가 되었던 연습법과 이미지!! 영상에 담아봤습니다.",
+    img: down04,
+    author: "김태승프로tv [RG골프스튜디오]",
+    videoId: "pr6CcBbXvYw",
+    date: "2023. 10. 20.",
+    channelId: "UCvTPttpo-lSO-UfuZpytKRw",
+  },
+  {
+    title:
+      "[아카데미 이시우] 발바닥의 움직임에 주목! 하체 회전을 만드는 연습 방법",
+    img: down05,
+    author: "SBS Golf",
+    videoId: "G8pHpTHZwR0",
+    date: "2021. 6. 30.",
+    channelId: "UC12XQWcIlaavjjbhh6EeMJw",
+  },
+  {
+    title: "[이시우 레슨] 올바른 하체의 움직임! 양 무릎의 공간을 좁게 만들기!",
+    img: down06,
+    author: "SBS Golf 레슨",
+    videoId: "UtgDehfeVlc",
+    date: "2023. 8. 16.",
+    channelId: "UCnY8QXU95EhOiPj30e9Maqg",
   },
 ];
