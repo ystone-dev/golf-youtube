eb591ca2c02baff5e95293e1b4d8ccf4105e03b8
이윤석 <ystone0213@naver.com>
2023-11-19 07:10:26 +0900

    feat: lessoner 데이터 추가

--- src/data/lessoner.js
+++ src/data/lessoner.js
@@ -5,4 +5,58 @@ export const lessonerText = [
     channelAddress: "https://www.youtube.com/@sbsgolf8202",
     channelId: "UCnY8QXU95EhOiPj30e9Maqg",
   },
+  {
+    img: "https://yt3.googleusercontent.com/jXx2UUpukTmblApdFmJu5SWJ4koxFFx6D7xqi8uuaGOme817rNMRDHgCXKOYTieI_5B3kJPWxQ=s176-c-k-c0x00ffffff-no-rj",
+    name: "[ER 골프] 이정웅프로",
+    channelAddress: "https://www.youtube.com/@ung_pro_",
+    channelId: "UCLdLy47SvLDgHkO4F1HcAWw",
+  },
+  {
+    img: "https://yt3.googleusercontent.com/DHRNlSBHsihQfFgJA0iIadWrERk02u_8w0f0Yoo1A_lenF8rOvNfNDHsEBNY6ZkGACZWQvFyww=s176-c-k-c0x00ffffff-no-rj",
+    name: "나연이즈백 LPGA Na Yeon Choi",
+    channelAddress: "https://www.youtube.com/@nayeonisback",
+    channelId: "UCaF15sSaClwGVpz-QV0ZzHw",
+  },
+  {
+    img: "https://yt3.googleusercontent.com/E71RStAwsZ6brE8cHRIYIibjmaNqga083EzhShkeBVbKu1AX9nGi8CgfIBy-AJrScEYzzBkm=s176-c-k-c0x00ffffff-no-rj",
+    name: "최민욱의 스마일골프",
+    channelAddress: "https://www.youtube.com/@smilegolf",
+    channelId: "UCTlAS_N5_SmgJqwF_o5jqfg",
+  },
+  {
+    img: "https://yt3.googleusercontent.com/ytc/APkrFKY36K10gqF5FjNXu5hu1HA2aWj1FENvW1t4Z7gCYw=s176-c-k-c0x00ffffff-no-rj",
+    name: "[감성골프] 최원진프로",
+    channelAddress: "https://www.youtube.com/@GamsungGolf",
+    channelId: "UCEBqiMfpvbXTh0X3ovc1a5w",
+  },
+  {
+    img: "https://yt3.googleusercontent.com/ytc/APkrFKYhdwRW9WautJaewZo6UIWg9uL0scz27p7Cqgv-=s176-c-k-c0x00ffffff-no-rj",
+    name: "국대골프",
+    channelAddress: "https://www.youtube.com/@user-yg6mz6vt6p",
+    channelId: "UC1xP40gA7K64YkFJF9BePaA",
+  },
+  {
+    img: "https://yt3.googleusercontent.com/hm1eAx98vA3LE-NvKG4uDLKLHr-T-ei8yovAosrK2UUYCFMjvTuXq2NvUPsCTD8fH43gVEus5A=s176-c-k-c0x00ffffff-no-rj",
+    name: "탈골스윙",
+    channelAddress: "https://www.youtube.com/@NBKGOLF",
+    channelId: "UCenz6VwVXmRFT-k5qNdT27w",
+  },
+  {
+    img: "https://yt3.googleusercontent.com/JX-Brey0mUp0L5O_2RLa-smi4DdMLHEZ6so_kYkazE5DnB2NnxVEeZts6mOMDO9LX5w458XWSQ=s176-c-k-c0x00ffffff-no-rj",
+    name: "박연습TV",
+    channelAddress: "https://www.youtube.com/@mio.golf_tv",
+    channelId: "UC6sVX_-lnuq8LqsD7Lp4WMg",
+  },
+  {
+    img: "https://yt3.googleusercontent.com/0k2dKvi2CQ_7L1nwFWaz2KIzwAPGovW5mj6AeeCJN4Q-ZQWUUX5TzPQDj_QFTNrKFW1EqMkM834=s176-c-k-c0x00ffffff-no-rj",
+    name: "SBS Golf",
+    channelAddress: "https://www.youtube.com/@SBSGolfnow",
+    channelId: "UC12XQWcIlaavjjbhh6EeMJw",
+  },
+  {
+    img: "https://yt3.googleusercontent.com/wm9vMuCsUghO2KHTYLNg9bQZ4MTJn9KYwqs9-OT0L-2GtEKhRXz92LJb07mNcOa-Ci0sVpf5C7o=s176-c-k-c0x00ffffff-no-rj",
+    name: "이보연프로 LBYGOLF",
+    channelAddress: "https://www.youtube.com/@lbygolf",
+    channelId: "UCRMRStGx4lDEhDHJb0LTTNA",
+  },
 ];
