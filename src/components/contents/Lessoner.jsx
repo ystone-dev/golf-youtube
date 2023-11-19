8b72696b4a92f699c9fb2af4d54d655f3182f70e
이윤석 <ystone0213@naver.com>
2023-11-20 02:32:08 +0900

    feat: lessoner 내 캐러셀 좌우버튼 마크업&스타일링

--- src/components/contents/Lessoner.jsx
+++ src/components/contents/Lessoner.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { lessonerText } from "../../data/lessoner";
 import { Link } from "react-router-dom";
+import left from "../../assets/img/left.svg";
+import right from "../../assets/img/right.svg";
 
 const Lessoner = () => {
   return (
     <section className="lessoner">
       <h2>👍🏻 추천 레스너</h2>
       <ul className="container overflow">
+        <button className="left">
+          <img src={left} alt="" />
+        </button>
         {lessonerText.map((lessoner, key) => (
           <li key={key}>
             <div className="img icon--play">
@@ -19,6 +24,9 @@ const Lessoner = () => {
             </div>
           </li>
         ))}
+        <button className="right">
+          <img src={right} alt="" />
+        </button>
       </ul>
     </section>
   );
