7259f0a453ad1ad52617ae345af498c87403b1ed
이윤석 <ystone0213@naver.com>
2023-11-22 01:43:34 +0900

    feat: lessoner 내 캐러셀 좌우버튼 기능 추가

--- src/components/contents/Lessoner.jsx
+++ src/components/contents/Lessoner.jsx
@@ -1,33 +1,48 @@
-import React from "react";
+import React, { useRef } from "react";
 import { lessonerText } from "../../data/lessoner";
 import { Link } from "react-router-dom";
 import left from "../../assets/img/left.svg";
 import right from "../../assets/img/right.svg";
 
 const Lessoner = () => {
+  const containerRef = useRef(null);
+
+  const handleXMove = (direction) => {
+    const container = containerRef.current;
+    if (container) {
+      container.scrollBy({
+        left: direction,
+        behavior: "smooth",
+      });
+    }
+  };
   return (
     <section className="lessoner">
       <h2>👍🏻 추천 레스너</h2>
-      <ul className="container overflow">
-        <button className="left">
+      <div className="container">
+        <ul className="inner overflow" ref={containerRef}>
+          {lessonerText.map((lessoner, key) => (
+            <li key={key} className="item">
+              <div className="img icon--play">
+                <Link to={`/channel/${lessoner.channelId}`}>
+                  <img src={lessoner.img} alt={lessoner.name} />
+                </Link>
+              </div>
+              <div className="info">
+                <Link to={`/channel/${lessoner.channelId}`}>
+                  {lessoner.name}
+                </Link>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <button className="left" onClick={() => handleXMove(-300)}>
           <img src={left} alt="" />
         </button>
-        {lessonerText.map((lessoner, key) => (
-          <li key={key}>
-            <div className="img icon--play">
-              <Link to={`/channel/${lessoner.channelId}`}>
-                <img src={lessoner.img} alt={lessoner.name} />
-              </Link>
-            </div>
-            <div className="info">
-              <Link to={`/channel/${lessoner.channelId}`}>{lessoner.name}</Link>
-            </div>
-          </li>
-        ))}
-        <button className="right">
+        <button className="right" onClick={() => handleXMove(300)}>
           <img src={right} alt="" />
         </button>
-      </ul>
+      </div>
     </section>
   );
 };
