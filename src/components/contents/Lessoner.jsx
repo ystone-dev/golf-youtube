4fae0be6f3677c4af20916465127db3496e636b0
이윤석 <ystone0213@naver.com>
2023-11-22 20:10:52 +0900

    feat: lessoner 내 캐러셀 좌우버튼 좌측끝, 우측끝 도달 시 각 버튼 사라지는 기능 추가

--- src/components/contents/Lessoner.jsx
+++ src/components/contents/Lessoner.jsx
@@ -1,26 +1,50 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { lessonerText } from "../../data/lessoner";
 import { Link } from "react-router-dom";
 import left from "../../assets/img/left.svg";
 import right from "../../assets/img/right.svg";
 
 const Lessoner = () => {
-  const containerRef = useRef(null);
+  const [leftEnd, setLeftEnd] = useState(false);
+  const [rightEnd, setRightEnd] = useState(true);
+  const innerRef = useRef(null);
+
+  useEffect(() => {
+    const inner = innerRef.current;
+
+    const handleScroll = () => {
+      if (inner) {
+        inner.scrollLeft === 0 ? setLeftEnd(false) : setLeftEnd(true);
+        inner.scrollLeft === 472 ? setRightEnd(false) : setRightEnd(true);
+      }
+    };
+
+    if (inner) {
+      inner.addEventListener("scroll", handleScroll);
+    }
+
+    return () => {
+      if (inner) {
+        inner.removeEventListener("scroll", handleScroll);
+      }
+    };
+  }, []);
 
   const handleXMove = (direction) => {
-    const container = containerRef.current;
-    if (container) {
-      container.scrollBy({
+    const inner = innerRef.current;
+    if (inner) {
+      inner.scrollBy({
         left: direction,
         behavior: "smooth",
       });
+      console.log(inner.scrollLeft);
     }
   };
   return (
     <section className="lessoner">
       <h2>👍🏻 추천 레스너</h2>
       <div className="container">
-        <ul className="inner overflow" ref={containerRef}>
+        <ul className="inner overflow" ref={innerRef}>
           {lessonerText.map((lessoner, key) => (
             <li key={key} className="item">
               <div className="img icon--play">
@@ -36,12 +60,16 @@ const Lessoner = () => {
             </li>
           ))}
         </ul>
-        <button className="left" onClick={() => handleXMove(-300)}>
-          <img src={left} alt="" />
-        </button>
-        <button className="right" onClick={() => handleXMove(300)}>
-          <img src={right} alt="" />
-        </button>
+        {leftEnd && (
+          <button className="left" onClick={() => handleXMove(-300)}>
+            <img src={left} alt="" />
+          </button>
+        )}
+        {rightEnd && (
+          <button className="right" onClick={() => handleXMove(300)}>
+            <img src={right} alt="" />
+          </button>
+        )}
       </div>
     </section>
   );
