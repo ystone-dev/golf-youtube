20cc0745449cb91cc6a775fd9a0fa4990398b096
이윤석 <ystone0213@naver.com>
2023-11-27 22:53:08 +0900

    feat: CarouselButton을 Video 컴포넌트에서도 쓰이게 수정

--- src/components/common/CarouselButton.jsx
+++ src/components/common/CarouselButton.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import left from "../../assets/img/left.svg";
 import right from "../../assets/img/right.svg";
 
-const CarouselButton = ({ direction, onScroll }) => {
+const CarouselButton = ({ direction, onScroll, onMove }) => {
   const handleXScroll = () => onScroll(direction === "left" ? -490 : 490);
+
+  const handleXMove = () => onMove();
+
   return (
     <button
       className={direction === "left" ? "left" : "right"}
-      onClick={handleXScroll}
+      onClick={onScroll ? handleXScroll : handleXMove}
     >
       <img src={direction === "left" ? left : right} alt="" />
     </button>
